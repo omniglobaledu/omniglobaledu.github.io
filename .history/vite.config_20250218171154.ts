d1779abdc4fb6a171e4ebbcd35a61fa7efbfa16b
omniglobaledu <omniglobaledu@gmail.com>
2025-02-18 17:16:20 +0545

    base

--- .history/vite.config_20250218171154.ts
+++ .history/vite.config_20250218171154.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/', // For GitHub Pages
+  base: '/omniglobaledu.github.io/', // For GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
